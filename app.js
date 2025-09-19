5e728bd523f44406bf03752799817ff18f911ccf
intanps072 <ips.intan072@gmail.com>
2025-09-19 20:37:36 +0700

    Done

--- app.js
+++ app.js
@@ -1,55 +1,42 @@
-const express = require ('express');
+const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const app = express ();
+const app = express();
 const port = 3000;
-// guna ejs
+
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.set('layout', 'layout/main-layout');
-app.get('/index', (req, res) => {
 
+app.get('/', (req, res) => {
     const mahasiswa = [
-        {   nama : 'Ade',
-            email : 'ade@gmail.com',  
-        },
-        {   nama : 'Asep',
-            email : 'asep@gmail.com',
-        },
-        {   nama : 'Agus', 
-            email : 'agus@gmail.com',
-        },
+        { nama: 'Ade', email: 'ade@gmail.com' },
+        { nama: 'Asep', email: 'asep@gmail.com' },
+        { nama: 'Agus', email: 'agus@gmail.com' },
     ];
 
-    res.render ('index', {
-        nama : 'Ade',
-        title : 'Halaman Home',
-        mahasiswa : mahasiswa,
+    res.render('index', {
+        nama: 'Ade',
+        title: 'Halaman Home',
+        mahasiswa: mahasiswa,
     });
-    });
+});
 
-app.get('/about', (req, res)=> {
-   // res.sendFile('./index.html', {root : __dirname});
-   res.render ('about', {title : 'Halaman About'}
-
-   );
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'Halaman About' });
 });
 
 app.get('/contact', (req, res) => {
-    // res.sendFile ('./about.html', {root : __dirname});
-    res.render ('contact', {title : 'Halaman Kontak'}
-    );
+    res.render('contact', { title: 'Halaman Kontak' });
 });
 
-app.get('/product', (req, res) => {
-    res.send (`product ID : ${req.params.id} <br> Category ID : ${req.params.idCat}`);
-        
+app.get('/product/:id/category/:idCat', (req, res) => {
+    res.send(`Product ID: ${req.params.id} <br> Category ID: ${req.params.idCat}`);
 });
 
-app.use ('/', (req, res) => {
-    res.status (404);
-    res.send ('<h1>Test</h1>');
+app.use((req, res) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(port, () => {
-    console.log(`Example apps listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 });
